e87cc4804e4a461ea2f0951470b81539561f88c4
Alistair <alistair@alistairbell.xyz>
2022-03-17 00:01:26 +0000

    Debug extension.
    Will output any errors thrown, really useful when debugging.
    Requires the GL_KHR_debug extension.
    
    Signed-off-by: Alistair <alistair@alistairbell.xyz>

--- src/main/pinkerton/ethan/graphics/renderer.java
+++ src/main/pinkerton/ethan/graphics/renderer.java
@@ -16,12 +16,22 @@
 
 package pinkerton.ethan.graphics;
 
-import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.GLCapabilities;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.KHRDebug;
+import org.lwjgl.opengl.GLDebugMessageCallback;
+import org.lwjgl.opengl.GLDebugMessageCallbackI;
 import pinkerton.ethan.window.window;
 import pinkerton.ethan.window.dimensions;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GLCapabilities;
+
+class debug_extension implements GLDebugMessageCallbackI {
+	@Override
+	public void invoke(int source, int type, int id, int severity, int length, long message, long userParam) {
+		if (severity == KHRDebug.GL_DEBUG_SEVERITY_HIGH || severity == KHRDebug.GL_DEBUG_SEVERITY_MEDIUM)
+			System.out.printf("error: GL callback message, %s.\n", GLDebugMessageCallback.getMessage(length, message));
+	}
+}
 
 public final class renderer {
 	public static renderer create(final window window_handle) {
@@ -43,7 +53,15 @@ public final class renderer {
 		GL20.glViewport(0, 0, dimensions.x[0], dimensions.y[0]);
 
 		System.out.printf("info:  GL version %s, %s.\n", GL20.glGetString(GL20.GL_VERSION), GL20.glGetString(GL20.GL_RENDERER));
+		result.handle_debug(capabilities);
 		return result;
+	}
+	public void handle_debug(GLCapabilities capabilities) {
+		if (capabilities.GL_KHR_debug) {
+			KHRDebug.glDebugMessageCallback(new debug_extension(), 0l);
+			GL20.glEnable(KHRDebug.GL_DEBUG_OUTPUT);
+			System.out.printf("info:  enabled debugging with GL_KHR_debug extension.\n");
+		}
 	}
 	public void clear() {
 		GL20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
@@ -54,7 +72,7 @@ public final class renderer {
 		vbo.bind();
 		vao.enable();
 		ibo.bind();
-		GL30.glDrawElements(GL30.GL_TRIANGLES, 6, GL30.GL_UNSIGNED_INT, 0);
+		GL20.glDrawElements(GL20.GL_TRIANGLES, 6, GL20.GL_UNSIGNED_INT, 0);
 	}
 
 }
